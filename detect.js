82ff22779647f476800e2eda1bfd08b1d13861e3
H A C K U R <coderinhtml@gmail.com>
2022-02-18 16:05:37 +0530

    Update detect.js

--- detect.js
+++ detect.js
@@ -105,10 +105,10 @@ class App extends React.Component {
           muted
           playsInline
           ref={this.videoRef}
-          width="500"
-          height="500"
+          width="720"
+          height="650"
         />
-        <canvas style={this.styles} ref={this.canvasRef} width="500" height="500" />
+        <canvas style={this.styles} ref={this.canvasRef} width="720" height="650" />
       </div>
     );
   }
